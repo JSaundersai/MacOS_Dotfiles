d3b887f5fd15ccbe5a1330fa556c5cb449d10ce7
JSaundersai <Jareds.ai@icloud.com>
2025-10-22 20:44:35 +1000

    added fonts and qute browser

---
+++ qutebrowser/config.py
@@ -0,0 +1,25 @@
+# Basic config for qutebrowser
+
+c.colors.webpage.darkmode.enabled = True
+
+c.url.start_pages = ["https://github.com"]
+
+c.tabs.background = True
+
+c.content.blocking.enabled = True
+
+c.content.blocking.method = 'both'
+
+c.scrolling.smooth = True
+
+c.editor.command = ['code', '{}']
+
+c.downloads.location.directory = '~/Downloads'
+
+c.hints.chars = 'asdfghjkl'
+
+c.keyhint.delay = 0
+
+c.tabs.show = 'switching'
+
+c.statusbar.show = 'in-mode'
